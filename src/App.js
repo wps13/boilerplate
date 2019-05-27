5dfe82aa8cf9afe195d49ddf6e8e25d5414c4a54
Willane Paiva <willane.paiva@codeminer42.com>
2019-05-27 11:49:15 -0300

    add store config in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React from "react";
 import "./App.css";
 
+import { Provider } from "react-redux";
+import createSagaMiddleware from "redux-saga";
+import { createStore, applyMiddleware, compose } from "redux";
+
+import Routes from "../routes";
+
+import rootSaga from "./store/sagas";
+import rootReducer from "./store/ducks";
+
+// eslint-disable-next-line no-underscore-dangle
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(sagaMiddleware)),
+);
+sagaMiddleware.run(rootSaga);
+
 const App = () => (
-  <div className="App">
-    <header className="App-header">Boilerplate</header>
-  </div>
+  <Routes>
+    <Provider store={store} />
+  </Routes>
 );
 
 export default App;
